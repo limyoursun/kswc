8441c07d4040f49f7bd44ca82c728912951b6e85
limyoursun <limyoursun@gmail.com>
2021-08-30 14:33:24 +0900

    top_side_상황판

--- src/js/ui_common.js
+++ src/js/ui_common.js
@@ -1,9 +1,14 @@
 $(function () {
+  $("#tabs").tabs();
+  $('.damage_press>li').click(function () {
+    $('.damage_press>li').removeClass('on');
+    $(this).addClass('on');
+  })
 
-  $('.damage_press>li').click(function(){
-                $('.damage_press>li').children('ul').slideUp('fast');
-                $(this).children('ul').slideDown('fast');
-                $('.damage_press>li').removeClass('on');
-                $(this).addClass('on');
-              })
+  $('.damage_press>li').click(function () {
+    $('.damage_press>li').children('ul').slideUp('fast');
+    $(this).children('ul').slideDown('fast');
+    $('.damage_press>li').removeClass('on');
+    $(this).addClass('on');
+  })
 })
