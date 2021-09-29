24b59302ccd4a46a6032b6dd623b58bee7353052
limyoursun <limyoursun@gmail.com>
2021-09-29 10:53:17 +0900

    done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ᓚᘏᗢ KSWC (The Korean Space Weather Center)
 우주전파센터 재난대응 시스템(내부)
+
+## SITE MAP
+퍼블리싱 단계에서 지정한 파일명입니다.
+작업에 참고하시길 바랍니다.
+![sitemap](https://user-images.githubusercontent.com/83810807/135005157-2c98c26d-688a-428f-9b0c-5678af82ef81.jpg)
--- src/js/ui_common.js
+++ src/js/ui_common.js
@@ -1,19 +1,88 @@
 $(function () {
-  $("#tabs").tabs();
-  $('.damage_press>li').click(function () {
-    $('.damage_press>li').removeClass('on');
-    $(this).addClass('on');
+  /*calendar*/
+  $.datepicker.setDefaults({
+    buttonImageOnly: true,
+    showOn: "both",
+    buttonImage: "../images/btn_calendar.png",
+    changeMonth: true,
+    changeYear: true,
+    numberOfMonths: 1,
+    regional: ["ko"],
+    dateFormat: "yy-mm-dd"
+  });
+  $("[dataformat='datepic']").datepicker({
+    buttonText: "날짜를 선택해주세요."
+  });
+  var from = $("[dataformat='from']").datepicker({
+    buttonText: "시작날짜를 선택해주세요.",
+    onClose: function (selectedDate) {
+      var getName = $(this).attr('name');
+      $("input[name='" + getName + "'].to").datepicker("option", "minDate", selectedDate);
+    }
+  });
+  var to = $("[dataformat='to']").datepicker({
+    buttonText: "종료날짜를 선택해주세요.",
+    onClose: function (selectedDate) {
+      var getName = $(this).attr('name');
+      $("input[name='" + getName + "'].from").datepicker("option", "maxDate", selectedDate);
+    }
+  });
+  
+  // pop
+  var popBtn = $('[openpop]');
+  popBtn.on('click', function () {
+    var target = $(this).attr('openpop');
+    $('#' + target).show();
   })
-
-  $('.damage_press>li').click(function () {
-    $('.damage_press>li').children('ul').slideUp('fast');
-    $(this).children('ul').slideDown('fast');
-    $('.damage_press>li').removeClass('on');
-    $(this).addClass('on');
+  var closePop = $('.btn_pop_close');
+  closePop.on('click', function () {
+    $(this).parents('.pop_overlay').hide();
   })
-
+  
   $('.con_list .more').on('click', function () {
     $(this).toggleClass('on');
     $(this).parent('p').nextAll('ul').slideToggle('fast');
   })
+
+  function gnb() {
+    var lm = $("#gnb");
+    lm.a = lm.find(">li>a");
+    lm.dep2 = lm.find(">li>ul");
+    lm.dep2.a = lm.dep2.find(">li>a");
+    lm.dep2.hide();
+    lm.a.each(function () {
+      if (!$(this).next().length) {
+        $(this).addClass("empty");
+      }
+      if ($(this).hasClass("on")) {
+        $(this).next("ul").show();
+      }
+    });
+    lm.a.on("click", function () {
+      lm.a.removeClass("on");
+      if ($(this).next("ul").is(":hidden")) {
+        lm.dep2.slideUp("fast");
+        $(this).next("ul").slideDown("fast");
+      } else {
+        $(this).removeClass("on");
+        $(this).next("ul").slideUp("fast");
+      }
+      // 클릭 후 슬라이드
+      var url = $(this).attr("href");
+      if ($(this).next("ul").is("ul")){
+        $(this).addClass('on');
+        $(this).next("ul").slideDown("fast", function () {
+          document.location.href = url;
+        });
+      } else if ($(this).next("ul").not("ul")) {
+        lm.dep2.slideUp('fast');
+        $(this).addClass("on");
+        setTimeout(function () {
+          document.location.href = url;
+        }, 200);
+      }
+      return false;
+    });
+  }
+  gnb();
 })
